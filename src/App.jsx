3749137b7ed2438275b1037379102bfa3af35b84
divanyym <divanydyna09@gmail.com>
2024-10-31 21:29:18 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import NavbarComponent from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import HomePage from "./pages/HomePage";
+import AboutPages from "./pages/AboutPages";
+import FaqPage from "./pages/FaqPage";
+import ExplorePage from "./pages/ExplorePage";
+import ServicesPage from "./pages/ServicesPage";
+
+
+function App() {
+   return (
+      <div>
+         <NavbarComponent/>
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/About" element={<AboutPages />} />
+            <Route path="/Services" element={<ServicesPage />} />
+            <Route path="/Explore" element={<ExplorePage />} />
+            <Route path="/faq" element={<FaqPage />} />
+         </Routes>
+
+         {/* <Footer/> */}
+      </div>
+   );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <header className="w-100 min-vh-100">
+        <div className="kelas w-100 min-vh-100"></div>
+      </header>
+    </div>
+  )
+}
+
+export default HomePage
